a2e4673e41401d807ec807196054d091ca10263c
ABILA <nahiltadesse01@gmail.com>
2025-08-30 12:40:15 +0300

    update search

--- app/Search/page.jsx
+++ app/Search/page.jsx
@@ -3,16 +3,20 @@ import React from "react";
 import { useSearchParams } from "next/navigation";
 import MovieGrid from "@/components/MovieGrid";
 import usePageTitle from "@/hooks/usePageTitle";
+
 const Search = () => {
   const searchParams = useSearchParams();
-
   const queryTerm = searchParams.get("q") || "";
+
   console.log("Query term:", queryTerm);
 
   const apiBase = "https://api.themoviedb.org/3/search/movie";
+
   usePageTitle(
-    query ? `Search: ${query} | moviehunter` : "Search | moviehunter"
+    queryTerm ? `Search: ${queryTerm} | moviehunter` : "Search | moviehunter"
   );
+
   return <MovieGrid apiBase={apiBase} queryTerm={queryTerm} />;
 };
+
 export default Search;
